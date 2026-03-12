b2325f41f32b3ca81649a0a593a8564ddff877ef
Darek F <dariusz.finster@gmail.com>
2026-03-12 21:24:28 +0100

    Add sortable columns to user mountain routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,23 @@ class UsersController < ApplicationController
 
     @user = Db::User.find_by(kw_id: params[:kw_id])
     @route_type_filter = params[:route_type].presence
+    @route_sort = params[:route_sort].presence || 'climbing_date'
+    @route_dir  = params[:route_dir].presence  || 'desc'
+
     all_routes = repository.fetch_mountain_routes(@user)
     all_routes = all_routes.select { |r| r.route_type == @route_type_filter } if @route_type_filter
+
+    all_routes = case @route_sort
+    when 'difficulty'
+      all_routes.sort_by { |r| (r.trad_climbing? || r.sport_climbing? ? r.kurtyka_difficulty.to_s : r.difficulty.to_s) }
+    when 'hearts_count'
+      all_routes.sort_by { |r| r.hearts_count.to_i }
+    else
+      all_routes.sort_by { |r| r.climbing_date || Date.new(1900) }
+    end
+
+    all_routes.reverse! if @route_dir == 'desc'
+
     @my_routes = Kaminari.paginate_array(all_routes).page(params[:route_page]).per(5)
     @my_courses = Kaminari.paginate_array(repository.fetch_courses(@user)).page(params[:course_page]).per(5)
     @my_projects = Kaminari.paginate_array(repository.fetch_projects(@user)).page(params[:project_page]).per(5)
